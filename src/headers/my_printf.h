0d9cdd2a7b74f4dd51123d7e63a9c098fef1f67e
Said AHEMT <said.ahemt@gmail.com>
2014-03-29 21:26:14 +0100

    finds identifier by id

--- src/headers/my_printf.h
+++ src/headers/my_printf.h
@@ -1,11 +1,12 @@
 #ifndef MY_PRINTF
   #define         MY_PRINTF
-
+  #define         IDENTIFIERS_COUNT 2
   typedef struct  s_identifier
   {
     char          id;
     void          (*func)();
   }               t_identifier;
-  void init_identifiers(t_identifier *);
-  void add_to_buffer(char, char *);
+  void            init_identifiers(t_identifier *);
+  void            add_to_buffer(char, char *);
+  int             identifier_match(char, t_identifier *, t_identifier *);
 #endif
--- src/my_printf.c
+++ src/my_printf.c
@@ -1,16 +1,20 @@
 #include "headers/my.h"
 #include "headers/my_printf.h"
+#include <stdio.h>
 
 void            my_printf(char *format, ...)
 {
   char          *buffer;
   int           i;
   int           identifier_found;
-  t_identifier  ids[2];
+  t_identifier  identifier;
+  t_identifier  *identifier_ptr;
+  t_identifier  ids[IDENTIFIERS_COUNT];
 
   buffer = malloc(sizeof(char));
   buffer[0] = '\0';
   identifier_found = 0;
+  identifier_ptr = &identifier;
   init_identifiers(ids);
   for (i = 0; format[i] != '\0'; i++)
   {
@@ -20,14 +24,22 @@ void            my_printf(char *format, ...)
     }
     else if (identifier_found)
     {
-      break;
+      if (identifier_match(format[i], identifier_ptr, ids))
+      {
+        my_putstr("et apres l'appel de identifier_match : ");
+        my_putchar(identifier.id);
+        my_putchar('\n');
+      }
+      else
+      {
+        my_putstr("Error: Unknown identifier\n");
+      }
     }
     else
     {
       add_to_buffer(format[i], buffer);
     }
   }
-  my_putstr(buffer);
   free(buffer);
 }
 
@@ -47,4 +59,21 @@ void            add_to_buffer(char c, char *buffer)
   buffer = realloc(buffer, sizeof(char));
   buffer[len] = c;
   buffer[(len + 1)] = '\0';
+}
+
+int             identifier_match(char id, t_identifier *identifier, t_identifier *ids)
+{
+  int           i;
+
+  for (i = 0; i < IDENTIFIERS_COUNT; i++)
+  {
+    if (id == ids[i].id)
+    {
+      printf("lorsque je trouve l'item, id = %c\n", ids[i].id);
+      identifier->id = ids[i].id;
+      identifier->func = ids[i].func;
+      return (1);
+    }
+  }
+  return (0);
 }
