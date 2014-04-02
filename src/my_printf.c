e34e7708ed336a02bc854de67f9280825ab0d724
Said AHEMT <said.ahemt@gmail.com>
2014-04-02 10:09:15 +0200

    adds identifiers checks

--- src/my_printf.c
+++ src/my_printf.c
@@ -3,43 +3,13 @@
 
 void            my_printf(char *format, ...)
 {
-  char          *buffer;
-  int           i;
-  int           identifier_found;
-  t_identifier  identifier;
-  t_identifier  *identifier_ptr;
   t_identifier  ids[IDENTIFIERS_COUNT];
 
-  buffer = init_buffer(NULL);
-  identifier_found = 0;
-  identifier_ptr = &identifier;
   init_identifiers(ids);
-  for (i = 0; format[i] != '\0'; i++)
+  if (identifiers_valid(format, ids))
   {
-    if (format[i] == '%')
-    {
-      identifier_found = 1;
-    }
-    else if (identifier_found)
-    {
-      if (identifier_match(format[i], identifier_ptr, ids))
-      {
-        my_putstr(buffer);
-        my_putchar('\n');
-        buffer = init_buffer(buffer);
-        identifier_found = 0;
-      }
-      else
-      {
-        my_putstr("Error: Unknown identifier\n");
-      }
-    }
-    else
-    {
-      add_to_buffer(format[i], buffer);
-    }
+    my_putstr("identifers are valid\n");
   }
-  free(buffer);
 }
 
 void            init_identifiers(t_identifier *ids)
@@ -58,7 +28,7 @@ void            add_to_buffer(char c, char *buffer)
   buffer = realloc(buffer, sizeof(char));
   if (buffer == NULL)
   {
-    perror("Error: ");
+    perror("my_printf: Error: ");
     free(buffer);
     exit(1);
   }
@@ -66,7 +36,7 @@ void            add_to_buffer(char c, char *buffer)
   buffer[(len + 1)] = '\0';
 }
 
-int             identifier_match(char id, t_identifier *identifier, t_identifier *ids)
+int             identifier_match(char id, t_identifier *ids, t_identifier *identifier)
 {
   int           i;
 
@@ -74,13 +44,62 @@ int             identifier_match(char id, t_identifier *identifier, t_identifier
   {
     if (id == ids[i].id)
     {
-      printf("lorsque je trouve l'item, id = %c\n", ids[i].id);
-      identifier->id = ids[i].id;
-      identifier->func = ids[i].func;
+      if (identifier != NULL)
+      {
+        printf("lorsque je trouve l'item, id = %c et que je passe un pointeur\n", ids[i].id);
+        identifier->id = ids[i].id;
+        identifier->func = ids[i].func;
+      }
       return (1);
     }
   }
   return (0);
+}
+
+int             identifiers_valid(char *format, t_identifier *ids)
+{
+  int           format_length;
+  int           i;
+  int           identifier_found;
+  int           valid;
+
+  format_length = my_strlen(format);
+  valid = 1;
+  for (i = 0; format[i] != '\0' && valid == 1; i++)
+  {
+    if (format[i] == '%')
+    {
+      if (i == (format_length - 1)) /* When % is the last char */
+      {
+        my_putstr("my_printf: Error: Undefined identifier in format string\n");
+        valid = 0;
+      }
+      else
+      {
+        identifier_found = 1;
+      }
+    }
+    else if (identifier_found)
+    {
+      if (format[i] == ' ')
+      {
+        my_putstr("my_printf: Error: Undefined identifier in format string\n");
+        valid = 0;
+      }
+      else if (identifier_match(format[i], ids, NULL) == 0)
+      {
+        my_putstr("my_printf: Error: Unknown identifier %");
+        my_putchar(format[i]);
+        my_putstr(" in format string\n");
+        valid = 0;
+      }
+      else
+      {
+        identifier_found = 0;
+      }
+    }
+  }
+  return (valid);
 }
 
 char            *init_buffer(char *old_buffer)
@@ -93,7 +112,7 @@ char            *init_buffer(char *old_buffer)
   buffer = malloc(sizeof(char));
   if (buffer == NULL)
   {
-    perror("Error: ");
+    perror("my_printf: Error: ");
     exit(1);
   }
   buffer[0] = '\0';
