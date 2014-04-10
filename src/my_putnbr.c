779b78c29fe65eef7a821c327e33ec837ea7503b
Said AHEMT <said.ahemt@gmail.com>
2014-04-10 08:26:07 +0200

    fills my_putnbr function. needs negative / positive issue check

--- src/my_putnbr.c
+++ src/my_putnbr.c
@@ -1,6 +1,29 @@
 #include  "headers/my.h"
 
-int       my_putnbr(int nbr)
+int       my_putnbr(int nb)
 {
-  return (nbr);
+  int           i;
+  int           figure;
+  char          current_char;
+  static char   zero_char = '0';
+
+  current_char = '0';
+  for (i = 0; i < 10; ++i)
+  {
+    figure = (nb < 0) ? -(nb % 10) : (nb % 10);
+    if (i == figure)
+    {
+      current_char = zero_char + i;
+      if (nb > 10 || nb < -10)
+      {
+        my_putnbr(nb / 10);
+      }
+      else if (nb < 0)
+      {
+        my_putchar('-');
+      }
+      my_putchar(current_char);
+    }
+  }
+  return (0);
 }
