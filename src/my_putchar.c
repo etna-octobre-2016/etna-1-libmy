85ff7d948b9cb6f8b759798b8404f9de5e9beeb9
Said AHEMT <said.ahemt@gmail.com>
2014-04-12 21:31:37 +0200

    changes my_putchar, my_putnbr, my_putstr return value

--- src/my_putchar.c
+++ src/my_putchar.c
@@ -1,7 +1,6 @@
 #include  "headers/my.h"
 
-int       my_putchar(int c)
+void      my_putchar(int c)
 {
   write(1, &c, 1);
-  return (1);
 }
--- src/my_putnbr.c
+++ src/my_putnbr.c
@@ -1,20 +1,18 @@
 #include        "headers/my.h"
 
-int             my_putnbr(int nb)
+void            my_putnbr(int nb)
 {
   int           i;
   int           figure;
-  int           output_length;
   char          current_char;
   static char   zero_char = '0';
 
   if (nb == INT_MIN)
   {
     my_putstr("my_putnbr: error: integer overflow\n");
-    return (-1);
+    return;
   }
   current_char = '0';
-  output_length = 0;
   for (i = 0; i < 10; ++i)
   {
     figure = (nb < 0) ? -(nb % 10) : (nb % 10);
@@ -23,14 +21,13 @@ int             my_putnbr(int nb)
       current_char = zero_char + i;
       if (nb > 10 || nb < -10)
       {
-        output_length += my_putnbr(nb / 10);
+        my_putnbr(nb / 10);
       }
       else if (nb < 0)
       {
-        output_length += my_putchar('-');
+        my_putchar('-');
       }
-      output_length += my_putchar(current_char);
+      my_putchar(current_char);
     }
   }
-  return (output_length);
 }
